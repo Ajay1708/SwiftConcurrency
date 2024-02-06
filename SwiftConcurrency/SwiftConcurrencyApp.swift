29f5eb4bfcb69b61606dec084005f5da09b97f27
Ajay <ajay.nellori@gmail.com>
2024-02-06 10:41:56 +0530

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Venkata Ajay Sai Nellori on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
