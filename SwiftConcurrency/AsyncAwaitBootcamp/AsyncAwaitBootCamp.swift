7851d599ed07b020e1b31f8afd001107b457e03c
Ajay <ajay.nellori@gmail.com>
2024-03-06 20:37:19 +0530

    Worked on Async Let, Task

--- SwiftConcurrency/AsyncAwaitBootcamp/AsyncAwaitBootCamp.swift
+++ SwiftConcurrency/AsyncAwaitBootcamp/AsyncAwaitBootCamp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct AsyncAwaitBootCamp: View {
     @StateObject var vm = AsyncAwaitBootCampViewModel()
+    @State private var addAuthorState: Task<(), Never>? = nil
     var body: some View {
         List(vm.dataArray, id: \.self) { item in
             Text(item)
@@ -16,11 +17,17 @@ struct AsyncAwaitBootCamp: View {
         .onAppear {
             vm.addTitle1()
             vm.addTitle2()
-            Task {
+            addAuthorState = Task {
                 await vm.addAuthor1()
+                //Just because we are awaiting something doesn't necessarily mean we are going onto a background thread
+                // Await is just a suspension point in the task we are awaiting the returned result
+                // So it is always best to explicitly use MainActor to switch to main thread before updating UI
             }
             let finalText = "FINAL TEXT: \(Thread.current)"
             vm.dataArray.append(finalText)
+        }
+        .onDisappear {
+            addAuthorState?.cancel()
         }
     }
 }
--- SwiftConcurrency/AsyncAwaitBootcamp/AsyncAwaitBootCampViewModel.swift
+++ SwiftConcurrency/AsyncAwaitBootcamp/AsyncAwaitBootCampViewModel.swift
@@ -32,11 +32,8 @@ class AsyncAwaitBootCampViewModel: ObservableObject {
     @MainActor func addAuthor1() async {
         let author1 = "Author1 : \(Thread.current)"
         dataArray.append(author1)
-        try? await Task.sleep(nanoseconds: 2_000_000_000) // Task.sleep executes in background thread so the immediate line will also execute in background thread. So we need to explicitly execute the below line in main thread because we are updaing UI.
+        try? await Task.sleep(nanoseconds: 2_000_000_000) // Sleep is an async function so we need to await for suspending the current task for atleast the given duration in seconds
         dataArray.append("Author2 : \(Thread.current)")
         
     }
-    //Just because we are awaiting something doesn't necessarily mean we are going onto a background thread
-    // Await is just a suspension point in the task we are awaiting the returned result
-    // So it is always best to explicitly use MainActor to switch to main thread before updating UI
 }
